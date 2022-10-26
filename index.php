80812ff2843e9e0ea89ec266dc08ce692df84f87
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-10-26 16:19:17 +0300

    create schema

---
+++ index.php
@@ -0,0 +1,11 @@
+Welcome to our Bluff game! 
+
+<?php
+echo "this line is from php...";
+$sql= "select * from CARDS";
+$st= $mysqli->prepare($sql);
+$st->execute();
+$res = $st->get_result();
+$r=$res->fetch_assoc();
+print "ID: $r[ID], CARD_TEXT: $r[card_text], CARD_SYMBOL: $r[card_symbol]";
+?>
--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -5,11 +5,15 @@ require_once "db_upass.php";
 
 $user=$DB_USER;
 $pass=$DB_PASS;
-if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli=new mysqli($host, $user, $pass, $db, null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
+if(gethostname()=='users.iee.ihu.gr')
+{
+$mysqli=new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
 } else {
-	$mysqli = new mysqli($host, $user, $pass, $db);
+$mysqli = new mysqli($host, $user, $pass, $db);
 }
+
+$mysqli = new mysqli($host, 'root', 'Fuegozz1!', $db);
+
 if ($mysqli->connect_errno) {
 	echo"Failed to connect to MySQL:(".
 	$mysqli->connect_errno . ") " . $mysqli->connect_error;
