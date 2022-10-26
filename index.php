b770b18c7b39782ce88d5ed8e8c0863ce0eafdf3
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-10-26 22:46:40 +0300

    index php solved for local

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-Welcome to our Bluff game! 
-
 <?php
-
-echo "this line is from php...";
-$sql= "select * from CARDS";
-$st= $mysqli->prepare($sql);
+$mysqli=new mysqli('localhost','root',null,'Bluff');
+if($mysqli->connect_errno) echo"failed to connect";
+$sql='select * from CARDS';
+$st=$mysqli->prepare($sql);
 $st->execute();
-$res = $st->get_result();
-$r=$res->fetch_assoc();
-print "ID: $r[ID], CARD_TEXT: $r[card_text], CARD_SYMBOL: $r[card_symbol]";
+$res= $st->get_result();
+$r= $res->fetch_assoc();
+print "CARDS_ID: $r[CARDS_ID], card_text: $r[card_text], card_symbol: $r[card_symbol]";
 ?>
--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,20 +1,6 @@
 <?php
-$host='localhost';
-$db='Bluff';
-require_once "db_upass.php";
 
-$user=$DB_USER;
-$pass=$DB_PASS;
-if(gethostname()=='users.iee.ihu.gr')
-{
-$mysqli=new mysqli($host, $user, $pass, $db,$pass,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
-} else {
-$mysqli = new mysqli($host, $user, $pass, $db);
-}
-
-$mysqli = new mysqli($host, 'root', 'Fuegozz1!', $db);
-
-if ($mysqli->connect_errno) {
-	echo"Failed to connect to MySQL:(".
-	$mysqli->connect_errno . ") " . $mysqli->connect_error;
-}?>
+$mysqli=new mysqli('localhost',root,null,'Bluff');
+if($mysqli->connect_errno) {
+	echo"failed to connect";
+?>
