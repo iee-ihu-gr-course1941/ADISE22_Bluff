e293283b884abd0114647d28e39aa07f3a5b9089
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-10-27 02:03:45 +0300

    hidden info

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,6 +1,12 @@
 <?php
 
-$mysqli=new mysqli('localhost',root,null,'Bluff');
-if($mysqli->connect_errno) {
-	echo"failed to connect";
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli=new mysqli('localhost','root','Fuegozz1!','Bluff',null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
+	echo "server side";	
+}
+else {
+	$mysqli=new mysqli('localhost','root',null,'Bluff');
+	echo "localhost side";
+}
+if($mysqli->connect_errno) echo"failed to connect";
 ?>
