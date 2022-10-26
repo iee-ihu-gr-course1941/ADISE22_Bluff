b0f74d0a8820bafe779817f0ef330cb19ba50418
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-10-27 01:41:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-$mysqli=new mysqli('localhost','root',null,'Bluff');
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli=new mysqli('localhost','root','Fuegozz1!','Bluff',null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
+	echo "server side";
+	
+}
+else {
+	$mysqli=new mysqli('localhost','root',null,'Bluff');
+	echo "localhost side";
+}
 if($mysqli->connect_errno) echo"failed to connect";
 $sql='select * from CARDS';
 $st=$mysqli->prepare($sql);
