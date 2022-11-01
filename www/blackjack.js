c69ef371f12a0c34395e0b581a632e0c1faca6e1
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-11-02 01:38:40 +0200

    updated index with ajax

--- www/blackjack.js
+++ www/blackjack.js
@@ -1,9 +1,11 @@
 
 window.onload = function() {
-    buildDeck();
-    
-
+    //buildDeck();
+    player1_cards();
+    player2_cards();
 }
+
+
 
 function buildDeck(){
 for (var i=0; i<24; i++)
@@ -21,6 +23,31 @@ image.src = url;
 document.getElementById('your-cards').appendChild(image);
 }
 }
-
+function player1_cards(){
+    $.ajax({url: "bluff.php/player1_cards", method :'get', success: display_p1cards});
+}
+function player2_cards(){
+  $.ajax({url: "bluff.php/player2_cards", method :'get', success: display_p2cards});
+}
+  function display_p1cards(data){
+    for(var i=0;i<data.length;i++)
+    {
+      var url = 'cards/'+data[i].PLAYER1_CARDS_ID+'.png'; 
+      var image = new Image();
+      image.src = url;
+      document.getElementById('dealer-cards').appendChild(image); 
+      
+    }
+  }  
+  function display_p2cards(data){
+    for(var i=0;i<data.length;i++)
+    {
+      var url = 'cards/'+data[i].PLAYER2_CARDS_ID+'.png'; 
+      var image = new Image();
+      image.src = url;
+      document.getElementById('your-cards').appendChild(image); 
+      
+    }
+  } 
 
 
