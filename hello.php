188ce56e12305ac1a306d2d3dd013b516c900496
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-11-27 22:50:17 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -10,4 +10,8 @@ else {
 $sql='select * from CARDS';
 $st=$mysqli->prepare($sql);
 $st->execute();
+$res= $st->get_result();
+$r= $res->fetch_assoc();
+print "ID: $r[ID], card_text: $r[card_text], card_symbol: $r[card_symbol]";
+?>
 ?>
