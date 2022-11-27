28fe46124b176d8d72052be190215fda77bdbbd4
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-11-27 23:40:11 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,6 +1,6 @@
 <?php
 if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli=new mysqli('users','root',null,'bluff',null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
+	$mysqli=new mysqli('localhost','root',null,'bluff',null,'/home/student/it/2017/it174988/mysql/run/mysql.sock');
 	echo "server side";	
 }
 else {
