acd6faa6b95e65205005cf821578a0939805827f
JohnThanassoulis <johnthanassoulis@hotmail.com>
2022-11-27 23:27:19 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -12,6 +12,6 @@ $st=$mysqli->prepare($sql);
 $st->execute();
 $res= $st->get_result();
 $r= $res->fetch_assoc();
-print "ID: $r[ID], card_text: $r[card_text], card_symbol: $r[card_symbol]";
+echo "ID: $r[ID], card_text: $r[card_text], card_symbol: $r[card_symbol]";
 ?>
 ?>
