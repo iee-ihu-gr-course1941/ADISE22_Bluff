8658482a504523d67484015a90b485376dd2ffd7
Nikolaos987 <73761101+Nikolaos987@users.noreply.github.com>
2023-01-07 23:34:45 +0200

    updated schema

--- MySchema.sql
+++ MySchema.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Διακομιστής:                  127.0.0.1
--- Έκδοση διακομιστή:            10.4.25-MariaDB - mariadb.org binary distribution
--- Λειτ. σύστημα διακομιστή:     Win64
--- HeidiSQL Έκδοση:              12.1.0.6537
+-- Host:                         127.0.0.1
+-- Server version:               10.4.25-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,10 +16,12 @@
 
 
 -- Dumping database structure for bluff
+DROP DATABASE IF EXISTS `bluff`;
 CREATE DATABASE IF NOT EXISTS `bluff` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `bluff`;
 
 -- Dumping structure for procedure bluff.bluff_check
+DROP PROCEDURE IF EXISTS `bluff_check`;
 DELIMITER //
 CREATE PROCEDURE `bluff_check`()
 BEGIN
@@ -32,38 +34,50 @@ DELETE FROM last_played;
 END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.board
+-- Dumping structure for table bluff.board
+DROP TABLE IF EXISTS `board`;
 CREATE TABLE IF NOT EXISTS `board` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `card_text` varchar(50) DEFAULT NULL,
   `card_symbol` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_BOARD_CARDS` (`ID`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=753 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bluff.board: ~0 rows (approximately)
+DELETE FROM `board`;
 
--- Dumping structure for πίνακας bluff.calls
+-- Dumping structure for table bluff.calls
+DROP TABLE IF EXISTS `calls`;
 CREATE TABLE IF NOT EXISTS `calls` (
   `total` int(11) DEFAULT NULL,
   `text` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table bluff.calls: ~1 rows (approximately)
+DELETE FROM `calls`;
+INSERT INTO `calls` (`total`, `text`) VALUES
+	(0, NULL);
 
 -- Dumping structure for procedure bluff.call_cards
+DROP PROCEDURE IF EXISTS `call_cards`;
 DELIMITER //
 CREATE PROCEDURE `call_cards`(
 	IN `totals` INT,
-	IN `texts` VARCHAR(50)
+	IN `texts` VARCHAR(50),
+	IN `turn` VARCHAR(50)
 )
 BEGIN
 DELETE FROM calls;
 INSERT INTO calls(total,TEXT) VALUES (totals,texts);
+
+UPDATE game_status
+	SET player_turn = turn;
 END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.cards
+-- Dumping structure for table bluff.cards
+DROP TABLE IF EXISTS `cards`;
 CREATE TABLE IF NOT EXISTS `cards` (
   `ID` int(11) NOT NULL,
   `card_text` varchar(50) DEFAULT NULL,
@@ -72,6 +86,7 @@ CREATE TABLE IF NOT EXISTS `cards` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bluff.cards: ~54 rows (approximately)
+DELETE FROM `cards`;
 INSERT INTO `cards` (`ID`, `card_text`, `card_symbol`) VALUES
 	(1, 'A', 'hearts'),
 	(2, '2', 'hearts'),
@@ -128,7 +143,33 @@ INSERT INTO `cards` (`ID`, `card_text`, `card_symbol`) VALUES
 	(53, 'joker', 'red'),
 	(54, 'joker', 'black');
 
+-- Dumping structure for procedure bluff.change_the_turn
+DROP PROCEDURE IF EXISTS `change_the_turn`;
+DELIMITER //
+CREATE PROCEDURE `change_the_turn`(
+	IN `turn` VARCHAR(50)
+)
+BEGIN
+
+	UPDATE game_status
+	SET player_turn = turn;
+
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure bluff.change_turn
+DROP PROCEDURE IF EXISTS `change_turn`;
+DELIMITER //
+CREATE PROCEDURE `change_turn`(
+	IN `playerturn` VARCHAR(50)
+)
+BEGIN
+
+END//
+DELIMITER ;
+
 -- Dumping structure for procedure bluff.dealcards
+DROP PROCEDURE IF EXISTS `dealcards`;
 DELIMITER //
 CREATE PROCEDURE `dealcards`()
 BEGIN
@@ -148,30 +189,35 @@ SELECT * FROM CARDS C WHERE  C.ID  NOT IN (SELECT C1.ID FROM PLAYER1_CARDS C1);
 	END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.game_status
+-- Dumping structure for table bluff.game_status
+DROP TABLE IF EXISTS `game_status`;
 CREATE TABLE IF NOT EXISTS `game_status` (
   `status` varchar(50) DEFAULT NULL,
-  `player_turn` int(11) DEFAULT NULL,
-  `result` int(11) DEFAULT NULL,
+  `player_turn` varchar(50) DEFAULT NULL,
+  `winner` varchar(50) DEFAULT NULL,
   `last_change` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bluff.game_status: ~0 rows (approximately)
+DELETE FROM `game_status`;
+INSERT INTO `game_status` (`status`, `player_turn`, `winner`, `last_change`) VALUES
+	('Hold', NULL, NULL, NULL);
 
--- Dumping structure for πίνακας bluff.last_played
+-- Dumping structure for table bluff.last_played
+DROP TABLE IF EXISTS `last_played`;
 CREATE TABLE IF NOT EXISTS `last_played` (
   `last_symbol` varchar(50) DEFAULT NULL,
   `last_text` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table bluff.last_played: ~0 rows (approximately)
+DELETE FROM `last_played`;
 
 -- Dumping structure for procedure bluff.newgame
+DROP PROCEDURE IF EXISTS `newgame`;
 DELIMITER //
 CREATE PROCEDURE `newgame`()
 BEGIN
-
-DELETE FROM users;
 
 DELETE  FROM PLAYER1_CARDS;
 
@@ -179,10 +225,23 @@ DELETE  FROM PLAYER2_CARDS;
 
 DELETE FROM board;
 
+UPDATE game_status
+SET 	
+	winner = NULL;
+	
+UPDATE game_status
+SET 	
+	player_turn = NULL;
+	
+UPDATE users
+SET 	
+	username = NULL;
+
 	END//
 DELIMITER ;
 
 -- Dumping structure for procedure bluff.pass
+DROP PROCEDURE IF EXISTS `pass`;
 DELIMITER //
 CREATE PROCEDURE `pass`()
 BEGIN
@@ -196,7 +255,8 @@ DELETE FROM BOARD;
 	END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.player1_cards
+-- Dumping structure for table bluff.player1_cards
+DROP TABLE IF EXISTS `player1_cards`;
 CREATE TABLE IF NOT EXISTS `player1_cards` (
   `ID` int(11) DEFAULT NULL,
   `card_text` varchar(50) DEFAULT NULL,
@@ -206,36 +266,38 @@ CREATE TABLE IF NOT EXISTS `player1_cards` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bluff.player1_cards: ~27 rows (approximately)
+DELETE FROM `player1_cards`;
 INSERT INTO `player1_cards` (`ID`, `card_text`, `card_symbol`) VALUES
-	(6, '6', 'hearts'),
-	(18, '5', 'spades'),
-	(27, 'A', 'diamonds'),
-	(12, 'Q', 'hearts'),
-	(40, 'A', 'clubs'),
-	(39, 'K', 'diamonds'),
-	(41, '2', 'clubs'),
 	(2, '2', 'hearts'),
-	(10, '10', 'hearts'),
-	(16, '3', 'spades'),
-	(29, '3', 'diamonds'),
-	(42, '3', 'clubs'),
-	(46, '7', 'clubs'),
-	(8, '8', 'hearts'),
-	(19, '6', 'spades'),
-	(52, 'K', 'clubs'),
-	(26, 'K', 'spades'),
-	(3, '3', 'hearts'),
-	(17, '4', 'spades'),
-	(9, '9', 'hearts'),
-	(25, 'Q', 'spades'),
-	(37, 'J', 'diamonds'),
-	(1, 'A', 'hearts'),
+	(23, '10', 'spades'),
+	(39, 'K', 'diamonds'),
 	(38, 'Q', 'diamonds'),
-	(28, '2', 'diamonds'),
-	(53, 'joker', 'red'),
-	(30, '4', 'diamonds');
+	(52, 'K', 'clubs'),
+	(40, 'A', 'clubs'),
+	(10, '10', 'hearts'),
+	(47, '8', 'clubs'),
+	(37, 'J', 'diamonds'),
+	(26, 'K', 'spades'),
+	(42, '3', 'clubs'),
+	(14, 'A', 'spades'),
+	(51, 'Q', 'clubs'),
+	(9, '9', 'hearts'),
+	(45, '6', 'clubs'),
+	(41, '2', 'clubs'),
+	(21, '8', 'spades'),
+	(24, 'J', 'spades'),
+	(54, 'joker', 'black'),
+	(1, 'A', 'hearts'),
+	(46, '7', 'clubs'),
+	(48, '9', 'clubs'),
+	(12, 'Q', 'hearts'),
+	(5, '5', 'hearts'),
+	(32, '6', 'diamonds'),
+	(7, '7', 'hearts'),
+	(31, '5', 'diamonds');
 
 -- Dumping structure for procedure bluff.player1_plays
+DROP PROCEDURE IF EXISTS `player1_plays`;
 DELIMITER //
 CREATE PROCEDURE `player1_plays`(
 	IN `text` VARCHAR(50),
@@ -245,14 +307,20 @@ BEGIN
 INSERT INTO board 
 SELECT 0,card_symbol,card_text FROM player1_cards WHERE card_symbol=symbol AND card_text=text;
 
-SELECT 0,card_symbol,card_text FROM player1_cards WHERE card_symbol=symbol AND card_text=TEXT;
+SELECT 0,card_symbol,card_text FROM player1_cards WHERE card_symbol=symbol AND card_text=text;
 
-DELETE FROM player1_cards WHERE card_symbol=symbol AND card_text=TEXT;
+DELETE FROM player1_cards WHERE card_symbol=symbol AND card_text=text;
+	
+-- UPDATE game_status
+-- SET 	
+--	player_turn = "B";
+
 SELECT * FROM player1_cards;
 END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.player2_cards
+-- Dumping structure for table bluff.player2_cards
+DROP TABLE IF EXISTS `player2_cards`;
 CREATE TABLE IF NOT EXISTS `player2_cards` (
   `ID` int(11) DEFAULT NULL,
   `card_text` varchar(50) DEFAULT NULL,
@@ -262,36 +330,38 @@ CREATE TABLE IF NOT EXISTS `player2_cards` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bluff.player2_cards: ~27 rows (approximately)
+DELETE FROM `player2_cards`;
 INSERT INTO `player2_cards` (`ID`, `card_text`, `card_symbol`) VALUES
+	(3, '3', 'hearts'),
 	(4, '4', 'hearts'),
-	(5, '5', 'hearts'),
-	(7, '7', 'hearts'),
+	(6, '6', 'hearts'),
+	(8, '8', 'hearts'),
 	(11, 'J', 'hearts'),
 	(13, 'K', 'hearts'),
-	(14, 'A', 'spades'),
 	(15, '2', 'spades'),
+	(16, '3', 'spades'),
+	(17, '4', 'spades'),
+	(18, '5', 'spades'),
+	(19, '6', 'spades'),
 	(20, '7', 'spades'),
-	(21, '8', 'spades'),
 	(22, '9', 'spades'),
-	(23, '10', 'spades'),
-	(24, 'J', 'spades'),
-	(31, '5', 'diamonds'),
-	(32, '6', 'diamonds'),
+	(25, 'Q', 'spades'),
+	(27, 'A', 'diamonds'),
+	(28, '2', 'diamonds'),
+	(29, '3', 'diamonds'),
+	(30, '4', 'diamonds'),
 	(33, '7', 'diamonds'),
 	(34, '8', 'diamonds'),
 	(35, '9', 'diamonds'),
 	(36, '10', 'diamonds'),
 	(43, '4', 'clubs'),
 	(44, '5', 'clubs'),
-	(45, '6', 'clubs'),
-	(47, '8', 'clubs'),
-	(48, '9', 'clubs'),
 	(49, '10', 'clubs'),
 	(50, 'J', 'clubs'),
-	(51, 'Q', 'clubs'),
-	(54, 'joker', 'black');
+	(53, 'joker', 'red');
 
 -- Dumping structure for procedure bluff.player2_plays
+DROP PROCEDURE IF EXISTS `player2_plays`;
 DELIMITER //
 CREATE PROCEDURE `player2_plays`(
 	IN `text` VARCHAR(50),
@@ -301,15 +371,29 @@ BEGIN
 INSERT INTO board 
 SELECT 0,card_symbol,card_text FROM player2_cards WHERE card_symbol=symbol AND card_text=text;
 
-SELECT 0,card_symbol,card_text FROM player2_cards WHERE card_symbol=symbol AND card_text=TEXT;
+SELECT 0,card_symbol,card_text FROM player2_cards WHERE card_symbol=symbol AND card_text=text;
 
+DELETE FROM player2_cards WHERE card_symbol=symbol AND card_text=text;
 
-DELETE FROM player2_cards WHERE card_symbol=symbol AND card_text=TEXT;
+-- UPDATE game_status
+-- SET 	
+--	player_turn = "A";
+
 SELECT * FROM player2_cards;
 END//
 DELIMITER ;
 
+-- Dumping structure for procedure bluff.play_card
+DROP PROCEDURE IF EXISTS `play_card`;
+DELIMITER //
+CREATE PROCEDURE `play_card`()
+BEGIN
+
+END//
+DELIMITER ;
+
 -- Dumping structure for procedure bluff.roundwin_P1
+DROP PROCEDURE IF EXISTS `roundwin_P1`;
 DELIMITER //
 CREATE PROCEDURE `roundwin_P1`()
 BEGIN
@@ -324,6 +408,7 @@ DELETE FROM BOARD;
 DELIMITER ;
 
 -- Dumping structure for procedure bluff.roundwin_P2
+DROP PROCEDURE IF EXISTS `roundwin_P2`;
 DELIMITER //
 CREATE PROCEDURE `roundwin_P2`()
 BEGIN
@@ -341,22 +426,60 @@ DELETE FROM BOARD;
 	END//
 DELIMITER ;
 
--- Dumping structure for πίνακας bluff.users
+-- Dumping structure for table bluff.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) DEFAULT NULL,
-  `playerno` varchar(50) DEFAULT NULL
+  `playerno` varchar(50) DEFAULT NULL,
+  `token` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table bluff.users: ~0 rows (approximately)
+-- Dumping data for table bluff.users: ~2 rows (approximately)
+DELETE FROM `users`;
+INSERT INTO `users` (`username`, `playerno`, `token`) VALUES
+	(NULL, 'A', '9521228aa06b602c4a0d12dc4f158ef4'),
+	(NULL, 'B', 'b8d8469376fec30516c745cc082022f2');
+
+-- Dumping structure for procedure bluff.win_check
+DROP PROCEDURE IF EXISTS `win_check`;
+DELIMITER //
+CREATE PROCEDURE `win_check`(
+	IN `player` CHAR(50)
+)
+BEGIN
+
+	UPDATE game_status
+	SET winner = player;
+	
+END//
+DELIMITER ;
 
 -- Dumping structure for trigger bluff.users_after_insert
+DROP TRIGGER IF EXISTS `users_after_insert`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
-CREATE TRIGGER `users_after_insert` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+CREATE TRIGGER `users_after_insert` AFTER UPDATE ON `users` FOR EACH ROW BEGIN
 DECLARE rowcount INT;
 DECLARE player1_cownt INT;
 DECLARE player2_cownt INT;
+
+DECLARE PlayersCount INT;
+
 SELECT COUNT(*) INTO rowcount FROM users;
+
+
+SELECT COUNT(*) INTO PlayersCount FROM users WHERE username IS NOT NULL;
+IF PlayersCount < 2 THEN
+	UPDATE game_status SET STATUS='Hold';
+ELSE
+	UPDATE game_status SET STATUS='Started';
+END IF;
+
+IF PlayersCount = 2 THEN
+	UPDATE game_status
+	SET player_turn = "A";
+END IF;
+
 IF rowcount  =0 
 then 
 CALL newgame;
@@ -370,6 +493,7 @@ DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 -- Dumping structure for trigger bluff.users_before_insert
+DROP TRIGGER IF EXISTS `users_before_insert`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
 CREATE TRIGGER `users_before_insert` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
